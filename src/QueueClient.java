dc443f1f29d22828b86dab71a6bf2263b9a79c1e
unknown <levantuan6898@gmail.com>
2020-04-22 18:46:36 +0700

    add file

---
+++ src/QueueClient.java
@@ -0,0 +1,15 @@
+public class QueueClient {
+    public static void main(String[] args) {
+        myQueue queue = new myQueue(4);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        System.out.println("========");
+        queue.show();
+        queue.dequeue();
+        queue.dequeue();
+        System.out.println("========");
+        queue.show();
+    }
+}
